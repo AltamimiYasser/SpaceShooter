24641e8fae0fd90fc61d2618fa93d4414ab1be31
altamimiyasser <altamimiy89@gmail.com>
2020-08-05 02:22:56 +0300

    Phase II: Framework :: Feature:New Enemy Movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+public enum EnemyMovementType
+{
+    Straight,
+    Angle
+}
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed = 4;
+    [SerializeField] private float _speed = 4;
 
-    [SerializeField]
-    private GameObject _laserPrefab;
+    [SerializeField] private GameObject _laserPrefab;
 
     private Player _player;
     private Animator _anim;
+    
+    private EnemyMovementType _movementType = EnemyMovementType.Straight;
 
     private AudioSource _explosionSound;
 
     private void Start()
+    {
+        SetupType();
+        InstantiateObjects();
+        StartCoroutine(Fire());
+    }
+
+    private void SetupType()
+    {
+        // pick a random type from enum
+        _movementType = (EnemyMovementType) Random.Range(0, System.Enum.GetValues(typeof(EnemyMovementType)).Length);
+    }
+
+    private void InstantiateObjects()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
         if (_player == null)
@@ -27,19 +48,35 @@ public class Enemy : MonoBehaviour
         _explosionSound = GetComponent<AudioSource>();
         if (_explosionSound == null)
             Debug.LogError("Explosion sound is null");
-
-        StartCoroutine(Fire());
     }
 
     // Update is called once per frame
     private void Update()
     {
-        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        Move();
+        Reposition();
+    }
 
+    private void Reposition()
+    {
         if (transform.position.y < -7.5)
         {
-            float newX = Random.Range(-9.5f, 9.5f);
+            var newX = Random.Range(-9.5f, 9.5f);
             transform.position = new Vector3(newX, 9.5f, 0);
+        }
+    }
+
+    private void Move()
+    {
+        if (_movementType == EnemyMovementType.Straight)
+        {
+            transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        else
+        {
+            var xDisplacement = transform.position.x > 0 ? -1 : 1;
+            var displacement = new Vector3(xDisplacement, -1, 0);
+            transform.Translate(displacement * _speed * Time.deltaTime);
         }
     }
 
@@ -52,7 +89,7 @@ public class Enemy : MonoBehaviour
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
             _speed *= 1.2f;
             _explosionSound.Play();
-            Destroy(this.gameObject, 2.5f);
+            Destroy(gameObject, 2.5f);
         }
 
         if (other.tag == "Laser" && other.GetComponent<Laser>().getCaller() == "Player")
@@ -63,7 +100,7 @@ public class Enemy : MonoBehaviour
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
             _speed *= 1.2f;
             _explosionSound.Play();
-            Destroy(this.gameObject, 2.5f);
+            Destroy(gameObject, 2.5f);
         }
     }
 
@@ -73,9 +110,9 @@ public class Enemy : MonoBehaviour
         {
             float random = Random.Range(3, 8);
             yield return new WaitForSeconds(random);
-            Vector3 offset = new Vector3(0, -1.05f, 0);
-            GameObject laserObject = Instantiate(_laserPrefab, transform.position + offset, Quaternion.identity);
-            Laser laser = laserObject.GetComponent<Laser>();
+            var offset = new Vector3(0, -1.05f, 0);
+            var laserObject = Instantiate(_laserPrefab, transform.position + offset, Quaternion.identity);
+            var laser = laserObject.GetComponent<Laser>();
             if (laserObject == null || laser == null)
                 Debug.LogError("Laser object or laser is null");
 
